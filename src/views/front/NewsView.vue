a96651a5137feb0e6b10f9aee25edfccb103b187
EmmaHsieh <z0905631590@gmail.com>
2024-02-27 00:07:28 +0800

    加上結尾標籤

--- src/views/front/NewsView.vue
+++ src/views/front/NewsView.vue
@@ -1,13 +1,13 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="4"></v-col>
+      <v-col cols="4">news</v-col>
     </v-row>
   </v-container>
 </template>
 
-<script>
-
+<script setup>
+</script>
 
 <style scoped>
 </style>
