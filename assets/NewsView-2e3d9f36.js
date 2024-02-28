da104d7bc573de7cb4eab226f9268eed03999423
EmmaHsieh76 <145088912+EmmaHsieh76@users.noreply.github.com>
2024-02-28 10:08:03 +0000

    Deploying to gh-pages from @ EmmaHsieh76/faji-front@d41b048cc47783d1d5716925f68540272fb39e5b 🚀

--- assets/NewsView-2e3d9f36.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,a2 as o,a4 as r,a5 as a,c as e,aa as s}from"./index-3755c69a.js";import{V as c}from"./VContainer-6ef582c8.js";import{a as n,V as l}from"./VRow-7d36bbe5.js";const _={};function f(d,i){return o(),r(c,null,{default:a(()=>[e(l,null,{default:a(()=>[e(n,{cols:"4"},{default:a(()=>[s("news")]),_:1})]),_:1})]),_:1})}const p=t(_,[["render",f]]);export{p as default};
