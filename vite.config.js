ea584e069ccdec3e0a3a9e605c4b65b2bc3f2314
EmmaHsieh <z0905631590@gmail.com>
2025-03-04 15:32:37 +0800

    處理preload警告-4

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,8 @@ export default defineConfig({
           name: 'Roboto',
           styles: 'wght@100;300;400;500;700;900'
         }]
-      }
+      },
+      preload:false
     }),
     VueDevTools()
   ],
