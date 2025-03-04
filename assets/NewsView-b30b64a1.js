5125a2f7c32886338ab5a46e85bb66891cf75808
EmmaHsieh76 <145088912+EmmaHsieh76@users.noreply.github.com>
2025-03-04 05:10:14 +0000

    Deploying to gh-pages from @ EmmaHsieh76/faji-front@896c286227193f737cd06246a28bf928573ddf88 🚀

--- assets/NewsView-b30b64a1.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,a2 as o,a4 as r,a5 as a,c as e,aa as s}from"./index-361666bb.js";import{V as c}from"./VContainer-2040e31a.js";import{a as n,V as l}from"./VRow-ce3b5e1e.js";const _={};function f(d,i){return o(),r(c,null,{default:a(()=>[e(l,null,{default:a(()=>[e(n,{cols:"4"},{default:a(()=>[s("news")]),_:1})]),_:1})]),_:1})}const p=t(_,[["render",f]]);export{p as default};
---
+++ assets/NewsView-c7f2f6eb.js
@@ -0,0 +1,1 @@
+import{_ as t,a2 as o,a4 as r,a5 as a,c as e,aa as s}from"./index-22136f92.js";import{V as c}from"./VContainer-bafbf90e.js";import{a as n,V as l}from"./VRow-ceb85e9c.js";const _={};function f(d,i){return o(),r(c,null,{default:a(()=>[e(l,null,{default:a(()=>[e(n,{cols:"4"},{default:a(()=>[s("news")]),_:1})]),_:1})]),_:1})}const p=t(_,[["render",f]]);export{p as default};
